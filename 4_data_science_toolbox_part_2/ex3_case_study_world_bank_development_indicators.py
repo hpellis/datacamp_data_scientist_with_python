88b085452dcc30eacfaf89bb530468dd6b702f6f
hpellis <harriet.p.ellis@gmail.com>
2019-03-04 21:08:43 +0000

    updating exercise

--- 4_data_science_toolbox_part_2/ex3_case_study_world_bank_development_indicators.py
+++ 4_data_science_toolbox_part_2/ex3_case_study_world_bank_development_indicators.py
@@ -23,8 +23,8 @@ rs_dict = dict(zipped_lists)
 print(rs_dict)
 
 
-#In this exercise, you will create a function to house the code you wrote earlier to make things easier and much more concise. 
-#This way, you only need to call the function and supply the appropriate lists to create your dictionaries! 
+#In this exercise, you will create a function to house the code you wrote earlier to make things easier and much more concise.
+#This way, you only need to call the function and supply the appropriate lists to create your dictionaries!
 #Again, the lists feature_names and row_vals are preloaded and these contain the header names of the dataset and actual values of a row from the dataset, respectively.
 #
 #Define the function lists2dict() with two parameters: first is list1 and second is list2.
@@ -55,7 +55,7 @@ print(rs_fxn)
 # Using list comprehension
 
 #This time, you're going to use the lists2dict() function you defined in the last exercise to turn a bunch of lists into a list of dictionaries with the help of a list comprehension.
-#The lists2dict() function has already been preloaded, together with a couple of lists, feature_names and row_lists. 
+#The lists2dict() function has already been preloaded, together with a couple of lists, feature_names and row_lists.
 #feature_names contains the header names of the World Bank dataset and row_lists is a list of lists, where each sublist is a list of actual values of a row from the dataset.
 #Your goal is to use a list comprehension to generate a list of dicts, where the keys are the header names and the values are the row entries.
 
@@ -73,7 +73,7 @@ print(list_of_dicts[1])
 
 # Creating a DataFrame
 
-#You will now use of all these to convert the list of dictionaries into a pandas DataFrame. 
+#You will now use of all these to convert the list of dictionaries into a pandas DataFrame.
 #You will see how convenient it is to generate a DataFrame from dictionaries with the DataFrame() function from the pandas package.
 #The lists2dict() function, feature_names list, and row_lists list have been preloaded for this exercise.
 
@@ -90,4 +90,55 @@ df = pd.DataFrame(list_of_dicts)
 print(df.head())
 
 
-# 
+#Processing data in chunks (1)
+#Sometimes, data sources can be so large in size that storing the entire dataset in memory becomes too resource-intensive.
+#In this exercise, you will process the first 1000 rows of a file line by line, to create a dictionary of the counts of how many times each country appears in a column in the dataset.
+
+#The csv file 'world_dev_ind.csv' is in your current directory for your use.
+#To begin, you need to open a connection to this file using what is known as a context manager.
+#For example, the command with open('datacamp.csv') as datacamp binds the csv file 'datacamp.csv' as datacamp in the context manager.
+#Here, the with statement is the context manager, and its purpose is to ensure that resources are efficiently allocated when opening a connection to a file.
+
+# Open a connection to the file
+with open('world_dev_ind.csv') as file:
+
+    # Skip the column names
+    file.readline()
+
+    # Initialize an empty dictionary: counts_dict
+    counts_dict = {}
+
+    # Process only the first 1000 rows
+    for j in range(1000):
+
+        # Split the current line into a list: line
+        line = file.readline().split(',')
+
+        # Get the value for the first column: first_col
+        first_col = line[0]
+
+        # If the column value is in the dict, increment its value
+        if first_col in counts_dict.keys():
+            counts_dict[first_col] += 1
+
+        # Else, add to the dict and set value to 1
+        else:
+            counts_dict[first_col] = 1
+
+# Print the resulting dictionary
+print(counts_dict)
+
+
+#Writing a generator to load data in chunks (2)
+#In the previous exercise, you processed a file line by line for a given number of lines.
+#What if, however, you want to do this for the entire file?
+
+#In this case, it would be useful to use generators.
+#Generators allow users to lazily evaluate data.
+#This concept of lazy evaluation is useful when you have to deal with very large datasets because it lets you generate values in an efficient manner by yielding only chunks of data at a time instead of the whole thing at once.
+
+#In this exercise, you will define a generator function read_large_file() that produces a generator object which yields a single line from a file each time next() is called on it.
+#The csv file 'world_dev_ind.csv' is in your current directory for your use.
+
+#Note that when you open a connection to a file, the resulting file object is already a generator! So out in the wild, you won't have to explicitly create generator objects in cases such as this.
+#However, for pedagogical reasons, we are having you practice how to do this here with the read_large_file() function. Go for it!

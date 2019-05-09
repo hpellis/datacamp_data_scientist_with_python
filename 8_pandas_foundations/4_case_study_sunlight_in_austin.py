7576da2fda6d80f77c9b6d8e6b3064864849d81d
hpellis <harriet.p.ellis@gmail.com>
2019-05-09 21:18:16 +0100

    completing sunlight in austin case study

--- 8_pandas_foundations/4_case_study_sunlight_in_austin.py
+++ 8_pandas_foundations/4_case_study_sunlight_in_austin.py
@@ -156,14 +156,184 @@ print(difference.mean())
 #The DataFrame df_clean from previous exercises has been provided for you.
 #The column 'sky_condition' provides information about whether the day was sunny ('CLR') or overcast ('OVC').
 
+
 # Using df_clean, when is sky_condition 'CLR'?
 is_sky_clear = df_clean['sky_condition']=='CLR'
 
 # Filter df_clean using is_sky_clear
-sunny = df_clear[is_sky_clear]
+sunny = df_clean.loc[is_sky_clear]
 
 # Resample sunny by day then calculate the max
 sunny_daily_max = sunny.resample('D').max()
 
 # See the result
 sunny_daily_max.head()
+
+
+#Get the cases in df_clean where the sky is overcast. Using .str.contains(), find when 'sky_condition' contains 'OVC', assigning to is_sky_overcast.
+#Use .loc[] to filter df_clean by is_sky_overcast, assigning to overcast.
+#Resample overcast by day ('D'), and take the max to find the maximum daily temperature.
+
+# Using df_clean, when does sky_condition contain 'OVC'?
+is_sky_overcast = df_clean['sky_condition'].str.contains('OVC')
+
+# Filter df_clean using is_sky_overcast
+overcast = df_clean.loc[is_sky_overcast]
+
+# Resample overcast by day then calculate the max
+overcast_daily_max = overcast.resample('D').max()
+
+# See the result
+overcast_daily_max.head()
+
+#Calculate the mean of sunny_daily_max, assigning to sunny_daily_max_mean.
+#Calculate the mean of overcast_daily_max, assigning to overcast_daily_max_mean.
+#Print sunny_daily_max_mean minus overcast_daily_max_mean. How much hotter are sunny days?
+
+# From previous steps
+is_sky_clear = df_clean['sky_condition']=='CLR'
+sunny = df_clean.loc[is_sky_clear]
+sunny_daily_max = sunny.resample('D').max()
+is_sky_overcast = df_clean['sky_condition'].str.contains('OVC')
+overcast = df_clean.loc[is_sky_overcast]
+overcast_daily_max = overcast.resample('D').max()
+
+# Calculate the mean of sunny_daily_max
+sunny_daily_max_mean = sunny_daily_max.mean()
+
+# Calculate the mean of overcast_daily_max
+overcast_daily_max_mean = overcast_daily_max.mean()
+
+# Print the difference (sunny minus overcast)
+print(sunny_daily_max_mean - overcast_daily_max_mean)
+
+
+
+###Weekly average temperature and visibility
+#Is there a correlation between temperature and visibility? Let's find out.
+
+#In this exercise, your job is to plot the weekly average temperature and visibility as subplots.
+#To do this, you need to first select the appropriate columns and then resample by week, aggregating the mean.
+
+#In addition to creating the subplots, you will compute the Pearson correlation coefficient using .corr().
+#The Pearson correlation coefficient, known also as Pearson's r, ranges from -1 (indicating total negative linear correlation) to 1 (indicating total positive linear correlation).
+#A value close to 1 here would indicate that there is a strong correlation between temperature and visibility.
+
+#The DataFrame df_clean has been pre-loaded for you.
+
+# Import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+
+# Select the visibility and dry_bulb_faren columns and resample them: weekly_mean
+weekly_mean = df_clean[['visibility', 'dry_bulb_faren']].resample('W').mean()
+
+# Print the output of weekly_mean.corr()
+print(weekly_mean.corr())
+
+# Plot weekly_mean with subplots=True
+weekly_mean.plot(subplots=True)
+plt.show()
+
+
+
+###Daily hours of clear sky
+#In a previous exercise, you analyzed the 'sky_condition' column to explore the difference in temperature on sunny days compared to overcast days.
+#Recall that a 'sky_condition' of 'CLR' represents a sunny day. In this exercise, you will explore sunny days in greater detail.
+#Specifically, you will use a box plot to visualize the fraction of days that are sunny.
+
+#The 'sky_condition' column is recorded hourly.
+#Your job is to resample this column appropriately such that you can extract the number of sunny hours in a day and the number of total hours.
+#Then, you can divide the number of sunny hours by the number of total hours, and generate a box plot of the resulting fraction.
+
+#As before, df_clean is available for you in the workspace.
+
+# Using df_clean, when is sky_condition 'CLR'?
+is_sky_clear = df_clean['sky_condition'] =='CLR'
+
+# Resample is_sky_clear by day
+resampled = is_sky_clear.resample('D')
+
+# See the result
+resampled
+
+#Calculate the number of measured sunny hours per day as the sum of resampled, assigning to sunny_hours.
+#Calculate the total number of measured hours per day as the count of resampled, assigning to total_hours.
+#Calculate the fraction of hours per day that were sunny as the ratio of sunny hours to total hours.
+
+
+# From previous step
+is_sky_clear = df_clean['sky_condition'] == 'CLR'
+resampled = is_sky_clear.resample('D')
+
+# Calculate the number of sunny hours per day
+sunny_hours = resampled.sum()
+
+# Calculate the number of measured hours per day
+total_hours = resampled.count()
+
+# Calculate the fraction of hours per day that were sunny
+sunny_fraction = sunny_hours/total_hours
+
+
+#Draw a box plot of sunny_fraction using .plot() with kind set to 'box'.
+
+# From previous steps
+is_sky_clear = df_clean['sky_condition'] == 'CLR'
+resampled = is_sky_clear.resample('D')
+sunny_hours = resampled.sum()
+total_hours = resampled.count()
+sunny_fraction = sunny_hours / total_hours
+
+# Make a box plot of sunny_fraction
+sunny_fraction.plot(kind='box')
+plt.show()
+
+
+###Heat or humidity
+#Dew point is a measure of relative humidity based on pressure and temperature.
+#A dew point above 65 is considered uncomfortable while a temperature above 90 is also considered uncomfortable.
+
+#In this exercise, you will explore the maximum temperature and dew point of each month.
+#The columns of interest are 'dew_point_faren' and 'dry_bulb_faren'. After resampling them appropriately to get the maximum temperature and dew point in each month, generate a histogram of these values as subplots.
+#Uncomfortably, you will notice that the maximum dew point is above 65 every month!
+
+#df_clean has been pre-loaded for you.
+
+
+# Resample dew_point_faren and dry_bulb_faren by Month, aggregating the maximum values: monthly_max
+monthly_max = df_clean[['dew_point_faren', 'dry_bulb_faren']].resample('M').max()
+
+# Generate a histogram with bins=8, alpha=0.5, subplots=True
+monthly_max.plot(kind='hist', bins=8, alpha=0.5, subplots=True)
+
+# Show the plot
+plt.show()
+
+
+
+###Probability of high temperatures
+#We already know that 2011 was hotter than the climate normals for the previous thirty years.
+#In this final exercise, you will compare the maximum temperature in August 2011 against that of the August 2010 climate normals.
+#More specifically, you will use a CDF plot to determine the probability of the 2011 daily maximum temperature in August being above the 2010 climate normal value.
+#To do this, you will leverage the data manipulation, filtering, resampling, and visualization skills you have acquired throughout this course.
+
+#The two DataFrames df_clean and df_climate are available in the workspace. Your job is to select the maximum temperature in August in df_climate, and then maximum daily temperatures in August 2011.
+#You will then filter out the days in August 2011 that were above the August 2010 maximum, and use this to construct a CDF plot.
+
+#Once you've generated the CDF, notice how it shows that there was a 50% probability of the 2011 daily maximum temperature in August being 5 degrees above the 2010 climate normal value!
+
+# Extract the maximum temperature in August 2010 from df_climate: august_max
+august_max = df_climate.loc['2010-Aug','Temperature'].max()
+print(august_max)
+
+# Resample August 2011 temps in df_clean by day & aggregate the max value: august_2011
+august_2011 = df_clean.loc['2011-Aug','dry_bulb_faren'].resample('D').max()
+
+# Filter for days in august_2011 where the value exceeds august_max: august_2011_high
+august_2011_high = august_2011.loc[august_2011 > august_max]
+
+# Construct a CDF of august_2011_high
+august_2011_high.plot(kind='hist', normed=True, cumulative=True, bins=25)
+
+# Display the plot
+plt.show()
